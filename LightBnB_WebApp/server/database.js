0c0e73176e801c658d9431c25a82daeabacf2b06
Robert Servado <servadorobertgmail.com>
2022-08-11 19:23:20 -0400

    Modified database.js specifically the getAllReservations function

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -88,7 +88,29 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  ////////////////////////////////////////////////////////////////////// getAllReservations
+  return pool
+    .query(
+      `SELECT reservations.*, properties.*
+      FROM reservations
+      JOIN properties ON properties.id = reservations.property_id
+      WHERE reservations.guest_id = $1
+      GROUP BY properties.id, reservations.id
+      ORDER BY reservations.start_date
+      LIMIT $2;`,
+      [guest_id, limit]
+    )
+    .then((result) => {
+      if (result) {
+        console.log(result.rows);
+        return result.rows;
+      } else {
+        return null;
+      }
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 exports.getAllReservations = getAllReservations;
 
